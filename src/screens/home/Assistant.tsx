0bbe133903fd22f9a81e62a2a7c67bf52c1d796e
hassankhan8323 <hassan.k@geeksofkolachi.com>
2023-08-31 03:03:04 +0500

    Fixed issue in search

--- src/screens/home/Assistant.tsx
+++ src/screens/home/Assistant.tsx
@@ -19,11 +19,13 @@ const CardList = () => {
   const navigate = useNavigate();
   const [resData, setResData] = React.useState<CombineRoomType[]>([]);
   const [filterData, setFilterData] = React.useState<CombineRoomType[]>([]);
+  const [isloading, setIsloading] = React.useState<boolean>(true)
   const { homeSearch, setHomeSearch } = useLayoutContext();
 
   const getAllAssistants = async () => {
     try {
       const data : CombineRoomType[] = await ChatService.listAssistants() || [];
+      setIsloading(false);
       setResData(data);
     } catch (error) {
       console.log('Fetch Error', error);
@@ -91,7 +93,7 @@ const CardList = () => {
   };
   const settings = {
     // dots: true,
-    infinite: true,
+    infinite: filterData.length > 3,
     slidesToShow: 5,
     slidesToScroll: 3,
     swipeToSlide: true,
@@ -142,7 +144,7 @@ const CardList = () => {
     ),
   };
   React.useEffect(() => {
-    if (homeSearch) {
+    if (resData.length && homeSearch) {
       const fiteredData = resData.filter((item) => {
         return item?.persona?.toLowerCase().includes(homeSearch.toLowerCase());
       });
@@ -154,11 +156,44 @@ const CardList = () => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [homeSearch]);
+
   return (
     <div>
-      {filterData.length ? (
+      {resData.length && !homeSearch ? (
         <Slider {...settings}>
-          {filterData.map((card) => {
+          {resData?.map((card) => {
+            return (
+              <div
+                key={card.persona}
+                className={`!w-[90%] relative !left-[5%]  h-48 xl:h-60 rounded-xl ${card.gradientColor} cursor-pointer`}
+                onClick={() =>
+                  navigate('/chat', {
+                    state: {
+                      uuid: card.uuid,
+                      cardName: card.persona,
+                    },
+                  })
+                }
+              >
+                <img
+                  src={card?.avatar?.replace(new RegExp(' ', 'g'), '_')}
+                  className='w-full p-2 h-32 xl:h-44'
+                />
+                <div className='flex justify-between'>
+                  <div className='m-2 font-semibold text-sm'>
+                    <p>{card.persona}</p>
+                  </div>
+                  <div className='mt-3 mx-3'>
+                    <Heart />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </Slider>
+      ) : homeSearch ? (
+        <Slider {...settings}>
+          {filterData?.map((card) => {
             return (
               <div
                 key={card.persona}
@@ -190,7 +225,7 @@ const CardList = () => {
         </Slider>
       ) : (
         <div className='h-48 flex justify-center text-slate-'>
-          {homeSearch} not found.
+          {isloading?'Loading...':`${homeSearch} not found.`}
         </div>
       )}
     </div>
